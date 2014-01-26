7cc8a6a73b726336e04d1649be8cf8ee2f74270f
Yihang Ho <hoyihang5@gmail.com>
2014-01-26 04:49:56 +0000

    Add eatsyscall.

---
+++ eatsyscall/eatsyscall.asm
@@ -0,0 +1,23 @@
+SECTION .data
+
+EatMsg: db "Eat at Joe's", 10
+EatLen: equ $-EatMsg
+
+SECTION .bss
+
+SECTION .text
+
+global _start
+
+_start:
+	nop
+
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, EatMsg
+	mov edx, EatLen
+	int 80H
+
+	mov eax, 1
+	mov ebx, 0
+	int 80H
